507c10f404f8d49741df3784f087dc1c3a724e1d
Michał Bogucki <bogucek4@gazeta.pl>
2019-10-04 11:02:02 +0200

    4 images

--- SourceCode/efficientnet_pytorch/effMain.py
+++ SourceCode/efficientnet_pytorch/effMain.py
@@ -30,16 +30,20 @@ def main():
     model = EfficientNet.pretrained(modelName)
 
     #for epoch in range(1, args.epochs + 1):
-    for epoch in range(1, 2 + 1):
+    for epoch in range(1, 2):
         # Open image
-        img = Image.open('img.jpg')
+        img1 = Image.open('img.jpg')
         img2 = Image.open('img2.jpg')
+        img3 = Image.open('img_200.jpg')
+        img4 = Image.open('img2_128.jpg')
         # Preprocess image
         tfms = transforms.Compose([transforms.Resize(imageSize), transforms.CenterCrop(imageSize),
                                    transforms.ToTensor(),
                                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]), ])
-        img = tfms(img).unsqueeze(0)
+        img1 = tfms(img1).unsqueeze(0)
         img2 = tfms(img2).unsqueeze(0)
+        img3 = tfms(img3).unsqueeze(0)
+        img4 = tfms(img4).unsqueeze(0)
 
         # Load class names
         labels_map = json.load(open('labels_map.txt'))
@@ -48,15 +52,19 @@ def main():
         # Classify with EfficientNet
         model.eval()
         with torch.no_grad():
-            logits = model(img)
+            logits1 = model(img1)
             logits2 = model(img2)
-        preds = torch.topk(logits, k=5).indices.squeeze(0).tolist()
+            logits3 = model(img3)
+            logits4 = model(img4)
+        preds1 = torch.topk(logits1, k=5).indices.squeeze(0).tolist()
         preds2 = torch.topk(logits2, k=5).indices.squeeze(0).tolist()
+        preds3 = torch.topk(logits2, k=5).indices.squeeze(0).tolist()
+        preds4 = torch.topk(logits2, k=5).indices.squeeze(0).tolist()
 
         print('-----')
-        for idx in preds:
+        for idx in preds1:
             label = labels_map[idx]
-            prob = torch.softmax(logits, dim=1)[0, idx].item()
+            prob = torch.softmax(logits1, dim=1)[0, idx].item()
             print('{:<75} ({:.2f}%)'.format(label, prob * 100))
 
         print('-----2')
@@ -64,6 +72,19 @@ def main():
             label = labels_map[idx]
             prob = torch.softmax(logits2, dim=1)[0, idx].item()
             print('{:<75} ({:.2f}%)'.format(label, prob * 100))
+
+        print('-----3')
+        for idx in preds3:
+            label = labels_map[idx]
+            prob = torch.softmax(logits3, dim=1)[0, idx].item()
+            print('{:<75} ({:.2f}%)'.format(label, prob * 100))
+
+        print('-----4')
+        for idx in preds4:
+            label = labels_map[idx]
+            prob = torch.softmax(logits4, dim=1)[0, idx].item()
+            print('{:<75} ({:.2f}%)'.format(label, prob * 100))
+
 
     showExecutionTime(startTime)
 
