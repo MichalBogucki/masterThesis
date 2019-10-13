a27395331ce497723f0b56b941d265a9aaee1320
Michał Bogucki <bogucek4@gazeta.pl>
2019-10-13 14:12:09 +0200

    gitignore

--- SourceCode/efficientnet_pytorch/effMain.py
+++ SourceCode/efficientnet_pytorch/effMain.py
@@ -18,9 +18,14 @@ def main():
     startTime = datetime.now()
 
     p = Augmentor.Pipeline("aug")
-    p.rotate(probability=1, max_left_rotation=25, max_right_rotation=25)
-    p.flip_left_right(probability=0.8)
-    p.sample(100)
+
+    p.skew(probability=0.5)
+    p.shear(probability=0.5, max_shear_left=10, max_shear_right=10)
+    p.random_distortion(probability=0.5, grid_width=8, grid_height=8, magnitude=2)
+    p.rotate(probability=0.5, max_left_rotation=15, max_right_rotation=15)
+    p.flip_left_right(probability=0.5)
+    p.zoom_random(probability=1, percentage_area=0.8)
+    p.sample(10000)
 
     print("augemented")
     return
