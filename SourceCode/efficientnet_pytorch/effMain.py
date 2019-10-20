1c6349eb9f46550bbfc1961450e062e76005baae
MichalBogucki_gtx1070 <bogucek4@gazeta.pl>
2019-10-20 21:58:42 +0200

    before writing TrainLoader

--- SourceCode/efficientnet_pytorch/effMain.py
+++ SourceCode/efficientnet_pytorch/effMain.py
@@ -1,8 +1,7 @@
 from __future__ import print_function
 import torch
-from torch.nn import functional as F
 
-from ManualFineTuneModel import ManualFineTuneModel
+from ImageFolderWithPaths import ImageFolderWithPaths
 #from ModelBinaryLayerAfterFC import ModelBinaryLayerAfterFC
 from compareImages import compareImages
 from setParserArguments import setParserArgumentsMnist
@@ -57,31 +56,16 @@ def main():
     #                                           batch_size=4, shuffle=True,
     #                                           num_workers=4)
     testLoader = torch.utils.data.DataLoader(testDataset,
-                                             batch_size=8, shuffle=False,
+                                             batch_size=8, shuffle=True,
                                              num_workers=4, pin_memory=True)
 
-    # #----
-    # model.eval()
-    # test_loss = 0
-    # correct = 0
-    # with torch.no_grad():
-    #     for data, target in testLoader:
-    #         data, target = data.to(device), target.to(device)
-    #         output = model(data)
-    #         # print("output")
-    #         # print(output)
-    #         # print("target")
-    #         # print(target)
-    #         test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-    #         pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-    #         correct += pred.eq(target.view_as(pred)).sum().item()
-    #         # print('Test set [x/y]: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'
-    #         #     .format(test_loss, correct, len(testLoader.dataset), 100. * correct / len(testLoader.dataset)))
-    #
-    # test_loss /= len(testLoader.dataset)
-    #
-    # print('Test set [x/y]: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'
-    #       .format(test_loss, correct, len(testLoader.dataset), 100. * correct / len(testLoader.dataset)))
+    # EXAMPLE USAGE:
+    # instantiate the dataset and dataloader
+    data_dir = "jpgImages/aug/test"
+    dataset = ImageFolderWithPaths(data_dir, transform=tfms)  # our custom dataset
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=True,
+                                             num_workers=4, pin_memory=True)
+
     #----
     # Open image
     nameViews = [
@@ -95,8 +79,7 @@ def main():
 
     #return visualizeGraphWithOnnxToNetron(model)
 
-    #compareImages(model, nameViews, tfms)
-
+    compareImages(model, nameViews, tfms)
     # ---------------------------------
 
     nameImg = [
@@ -111,8 +94,7 @@ def main():
         'pies.jpg'
         ]
 
-    #compareImages(model, nameImg, tfms)
-
+    compareImages(model, nameImg, tfms)
     # ---------------------------------
 
     # ---Binary---
@@ -120,23 +102,22 @@ def main():
     labels_map = [labels_map[str(i)] for i in range(2)]
     # Classify with EfficientNet
     with torch.no_grad():
-        for data, target in testLoader:
-            print("DUPA")
-            print(target)
+        for data, target, paths in dataloader:
             data, target = data.to(device), target.to(device)
+            print(target)
             logits1 = model(data)
+            index = 0
             for item in logits1:
-                # print(item.shape)
-                # print(item)
+                #print(paths[index])
+                #index += 1
                 preds1 = torch.topk(item, k=2).indices.squeeze(0).tolist()
-                # print(preds1)
                 print('-----')
                 for idx in preds1:
-                    # print(idx)
                     label = labels_map[idx]
                     prob = torch.softmax(item, dim=0)[idx].item()
                     print('{:<75} ({:.2f}%)'.format(label, prob * 100))
     # ---Binary---
+
 
     #---labels_map---
     labels_map = json.load(open('labels_map.txt'))
@@ -147,35 +128,13 @@ def main():
     model.eval()
     with torch.no_grad():
         logits1 = model(img1)
-        print(logits1.shape)
         preds1 = torch.topk(logits1, k=2).indices.squeeze(0).tolist()
-        print(logits1)
-        print(preds1)
         print('-----')
         for idx in preds1:
-            print(idx)
             label = labels_map[idx]
             prob = torch.softmax(logits1, dim=1)[0, idx].item()
             print('{:<75} ({:.2f}%)'.format(label, prob * 100))
     # ---labels_map---
-
-    # print('-----2')
-    # for idx in preds2:
-    #     label = labels_map[idx]
-    #     prob = torch.softmax(logits2, dim=1)[0, idx].item()
-    #     print('{:<75} ({:.2f}%)'.format(label, prob * 100))
-    #
-    # print('-----3')
-    # for idx in preds3:
-    #     label = labels_map[idx]
-    #     prob = torch.softmax(logits3, dim=1)[0, idx].item()
-    #     print('{:<75} ({:.2f}%)'.format(label, prob * 100))
-    #
-    # print('-----4')
-    # for idx in preds4:
-    #     label = labels_map[idx]
-    #     prob = torch.softmax(logits4, dim=1)[0, idx].item()
-    #     print('{:<75} ({:.2f}%)'.format(label, prob * 100))
 
     showExecutionTime(startTime)
 
