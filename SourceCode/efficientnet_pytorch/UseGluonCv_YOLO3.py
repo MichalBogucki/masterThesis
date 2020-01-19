a3bd8b4cd59bdbd5b13c68c77da81087f04ca73b
Michał Bogucki <bogucek4@gazeta.pl>
2020-01-19 18:42:30 +0100

    taking as array

--- SourceCode/efficientnet_pytorch/UseGluonCv_YOLO3.py
+++ SourceCode/efficientnet_pytorch/UseGluonCv_YOLO3.py
@@ -23,43 +23,52 @@ def UseGluonCv_YOLO3():
     startTime = datetime.now()
     result = glob.glob('example' + '/**/*.jpg', recursive=True)
     iter = 0
-
+    imageTensor2, imgToDisplay2 = data.transforms.presets.yolo.load_test(result)
+    #imList = mx.nd.array(imageTensor2)
+    #print(type(imList))
     print('len: ', len(result))
+    #for item in range(len(result)):
+    #    imageTensor2[item] = imageTensor2[item].as_in_context(ctx)  # Todo MOVE TO GPU
+
     for resultItem in result:
         print('GPU resultItem: ', resultItem)
         # DiskLocation - C:\Users\Michał\.mxnet\models
         threshold = 0.2
 
         #imageTensor, imgToDisplay = data.transforms.presets.yolo.load_test('Lotr2.jpg', short=1024)
-        imageTensor, imgToDisplay = data.transforms.presets.yolo.load_test(resultItem)#, short=1024)
+        #imageTensor, imgToDisplay = data.transforms.presets.yolo.load_test(resultItem)#, short=1024)
+
         #print('imageTensor.shape', imageTensor.shape)
         #print('imgToDisplay.shape', imgToDisplay.shape)
+        #print('imageTensor2.shape', imageTensor2[iter].shape)
+        #print('imgToDisplay2.shape', imgToDisplay2[iter].shape)
         # print('coco classes: ', net.classes)
         net.reset_class(classes=['person'], reuse_weights=['person'])
         # now net has 2 classes as desired
         # print('new classes: ', net.classes)
         #print('Shape of pre-processed image:', imageTensor.shape)
-
-        imageTensor = imageTensor.as_in_context(ctx) #Todo MOVE TO GPU
-        class_IDs, scores, bounding_boxs = net(imageTensor)
-
+        imageTensor2[iter] = imageTensor2[iter].as_in_context(ctx) #Todo MOVE TO GPU
+        class_IDs, scores, bounding_boxs = net(imageTensor2[iter])
 
         thresholdIndex = next(x[0] for x in enumerate(scores[0]) if x[1] < threshold)
         filteredScores = scores[0][:thresholdIndex]
         if (len(filteredScores)==0):
+            iter += 1
             continue
         filteredClassIds = class_IDs[0][:thresholdIndex]
         filteredBoundingBoxes = bounding_boxs[0][:thresholdIndex]
         #print(filteredScores)
         #print(filteredBoundingBoxes)
         # # print(net.classes)
-        # ax = utils.viz.plot_bbox(imgToDisplay, filteredBoundingBoxes, filteredScores,
-        #            filteredClassIds, class_names=net.classes, thresh=threshold)
-        iter += 1
-        #plt.savefig('plot_{}.jpg'.format(iter))
-        #plt.close()
+        ax = utils.viz.plot_bbox(imgToDisplay2[iter], filteredBoundingBoxes, filteredScores,
+                    filteredClassIds, class_names=net.classes, thresh=threshold)
+
+        plt.savefig('plot_{}.jpg'.format(iter))
+        plt.close()
         print('saved')
+        iter += 1
         #plt.show()
+
 
     print('len: ', len(result))
     showExecutionTime(startTime)
